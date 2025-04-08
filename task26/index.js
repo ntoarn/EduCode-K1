54659f3cfcd4c48aa7adeb6f94f3e3c7c74fe8f1
ntoarn <nqton301004@gmail.com>
2025-04-08 23:16:15 +0700

    for task26

--- task26/index.js
+++ task26/index.js
@@ -1,21 +1,51 @@
+// const fruits = [
+//     "apple",
+//     "banana",
+//     "kiwi",
+//     "kiwi",
+//     "banana",
+//     "orange",
+//     "apple",
+//     "kiwi",
+//   ];
+  
+//   function countElement(arr) {
+//     // Xử lý và in ra kết quả
+//   }
+  
+//   const result = fruits.reduce((acc, fruits) => {
+//     acc[fruits] = (acc[fruits] || 0) + 1;
+//     return acc;
+//   }, {});
+  
+//   console.log(result); // { apple: 2, banana: 2, kiwi: 3, orange: 1 }
 const fruits = [
-    "apple",
-    "banana",
-    "kiwi",
-    "kiwi",
-    "banana",
-    "orange",
-    "apple",
-    "kiwi",
-  ];
-  
-  function countElement(arr) {
-    // Xử lý và in ra kết quả
+  "apple",
+  "banana",
+  "kiwi",
+  "kiwi",
+  "banana",
+  "orange",
+  "apple",
+  "kiwi",
+];
+
+function countElement(arr) {
+  const result = {}
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] !== null){
+      for(let j = 0; j < arr.length; j++){
+        if(arr[i] === arr[j] ){
+          result[arr[i]] = (result[arr[i]] || 0) + 1;
+          arr[j] = null;
+        }
+      }
+    }
   }
-  
-  const result = fruits.reduce((acc, fruits) => {
-    acc[fruits] = (acc[fruits] || 0) + 1;
-    return acc;
-  }, {});
-  
-  console.log(result); // { apple: 2, banana: 2, kiwi: 3, orange: 1 }
+  return result
+  // Xử lý và in ra kết quả
+}
+
+const result = countElement(fruits);
+
+console.log(result); // { apple: 2, banana: 2, kiwi: 3, orange: 1 }
